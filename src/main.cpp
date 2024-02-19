cd181fb67aa67a275648dda9da3d924c6a46895d
Wouter Van de Wiele <wvandewiele@gmail.com>
2024-02-19 01:10:10 +0100

    make a wrapper for the SED1530 driver so that the updateWholeScreen runs in a task.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
-// #include "lcd_config.h"
+#include "lcd_config.h"
 // #include "debug_cli.h"
-#include "audio.h"
+// #include "audio.h"
 
 void setup() {
     Serial.begin(115200);
-    // lcd_setup();
+    lcd_setup();
     // debug_cli_setup();
-    audio_setup();
+    // audio_setup();
 }
 
 void loop() {
-    // lcd_test();
+    lcd_test();
     // debug_cli_loop();
-    delay(100);
+    // delay(100);
 }
