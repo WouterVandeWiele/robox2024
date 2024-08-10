bbcdac252540453939cf27c80e7616f81d455040
Wouter Van de Wiele <wvandewiele@gmail.com>
2024-08-10 18:56:19 +0200

    test server esp webtools software update

---
+++ test_settings_server/esp_upload.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index2.html", context={}
+    )
