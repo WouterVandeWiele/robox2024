a62ff13cf7a44786c6aed2279ed3c901b18d355b
Wouter Van de Wiele <wvandewiele@gmail.com>
2024-09-28 20:32:52 +0200

    frontend prototyping

--- test_settings_server/main.py
+++ test_settings_server/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form, Response
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from typing import Annotated
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
+_station_list = [
+    "http://link1",
+    "http://link2",
+    "http://link3"
+]
 
 
 @app.get("/")
@@ -14,3 +21,55 @@ def index(request: Request):
     return templates.TemplateResponse(
         request=request, name="index.html", context={}
     )
+
+
+@app.get("/station_list")
+def station_list_html(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="station_list.html",
+        context={"url_list": _station_list}
+    )
+
+
+@app.delete("/station_list")
+def delete_station(request: Request, id: int):
+    _station_list.pop(id-1)
+    return templates.TemplateResponse(
+            request=request,
+            name="station_list.html",
+            context={"url_list": _station_list},
+            headers={"HX-Location": '{"path":"/station_list", "target":"#station_table"}'}
+        )
+
+
+@app.post("/station_list")
+def add_station(request: Request, url: Annotated[str, Form()]):
+    _station_list.append(url)
+    return templates.TemplateResponse(
+            request=request,
+            name="station_list.html",
+            context={"url_list": _station_list},
+            headers={"HX-Location": '{"path":"/station_list", "target":"#station_table"}'}
+        )
+
+
+@app.put("/station_list")
+def update_station(request: Request, response: Response, id: int, url: Annotated[str, Form()]):
+    _station_list[id-1] = url
+    response.headers["HX-Location"] = '{"path":"/station_list", "target":"#station_table"}'
+#     return templates.TemplateResponse(
+#             request=request,
+#             name="station_row.html",
+#             context={"count": id, "item": _station_list[id-1]}
+#         )
+
+
+@app.get("/url_edit")
+def edit_template(request: Request, id: int):
+    return templates.TemplateResponse(
+            request=request,
+            name="row_edit.html",
+            context={"id": id, "url": _station_list[id-1]},
+            # headers={"HX-Location": '{"path":"/station_list", "target":"#station_table"}'}
+        )
